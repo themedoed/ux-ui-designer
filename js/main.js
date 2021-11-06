c903df479ccc28172ed1b5782a7eb9697d6aa49d
themedoed <93190084+themedoed@users.noreply.github.com>
2021-11-06 19:48:12 +0700

    Сделал адаптив меню

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
 $(function(){
-  $('.slider').slick({
-    prevArrow: '<button type="button" class="slick-prev"><img src="images/Arrow-left.svg" alt="Left-Arrow"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/Arrow-right.svg" alt="Right-Arrow"></button>'
-  }); 
+    $('.home__slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
 
-  $('.menu__btn').on('click', function(){
-      $('.menu__items').slideToggle();
-  });
+    $('.skills__slider').slick({
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 2000
+    })
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__inner').slideToggle();
+    });
 });
+
+
+
+// Плавные якоря
+const anchors = document.querySelectorAll('.menu__list a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+  e.preventDefault()
+   
+  const blockID = anchor.getAttribute('href').substr(1)
+   
+  document.getElementById(blockID).scrollIntoView({
+  behavior: 'smooth',
+  block: 'start'
+  })
+  })
+}
