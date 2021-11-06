bdc18dda37d6e6ee57dea07d74f00f60f3359ebf
themedoed <93190084+themedoed@users.noreply.github.com>
2021-11-06 07:01:27 +0700

    Сверстал вэб

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(function(){
+    $('.home__slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    $('.skills__slider').slick({
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 2000
+    })
+});
+
+
+
+// Плавные якоря
+const anchors = document.querySelectorAll('.menu__list a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+  e.preventDefault()
+   
+  const blockID = anchor.getAttribute('href').substr(1)
+   
+  document.getElementById(blockID).scrollIntoView({
+  behavior: 'smooth',
+  block: 'start'
+  })
+  })
+}
